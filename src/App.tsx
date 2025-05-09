43ccf3fb8f3c63a6d645ce68fb06d0dfe1cdd367
[dyad] <git@dyad.sh>
2025-05-09 13:27:13 +0200

    [dyad] Accès direct au traducteur d'emojis - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import EmojiTranslatorPage from "./components/EmojiTranslatorPage"; // Changed path
+import EmojiTranslatorPage from "./components/EmojiTranslatorPage";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +15,12 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<EmojiTranslatorPage />} />
+          {/* L'ancienne route /emoji-translator peut être conservée ou supprimée, 
+              car / y mène maintenant directement. Pour la simplicité, nous la laissons 
+              au cas où il y aurait des liens directs existants, mais elle est redondante. 
+              Si vous préférez la supprimer, faites-le moi savoir.
+          */}
           <Route path="/emoji-translator" element={<EmojiTranslatorPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
